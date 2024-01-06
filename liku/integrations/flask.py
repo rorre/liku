cc642a610168c35a59ee9fbff1f866c147f2f948
Rendy Arya Kemal <renrror@gmail.com>
2024-01-06 09:58:55 +0700

    Use make_response for flask integration

--- liku/integrations/flask.py
+++ liku/integrations/flask.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from typing import Callable, Mapping, Sequence, Tuple, Union
-from flask import Response
+from flask import Response, make_response
 from liku.elements import HTMLElement
 
 
@@ -28,25 +28,9 @@ def component_response[T: ResponseReturnValue, **P](
         result = f(*args, **kwargs)
 
         if isinstance(result, HTMLElement):
-            return Response(str(result), content_type="text/html")
+            return make_response(str(result))
 
-        html_str = str(result[0])
-        headers: HeadersValue = {}
-        status = 200
-        if len(result) == 2:
-            if isinstance(result[1], int):
-                status = result[1]
-            else:
-                headers = result[1]
-        else:
-            status = result[1]
-            headers = result[2]
-
-        return Response(
-            html_str,
-            headers=headers,
-            status=status,
-            content_type="text/html",
-        )
+        component, *rest = result
+        return make_response(str(component), *rest)
 
     return wrapper
